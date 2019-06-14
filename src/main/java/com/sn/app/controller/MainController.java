15e84b9a4bb35a53aaea4472b19afcf363b0cdbb
SnYoussef <snyoussef07@gmail.com>
2019-06-14 23:31:02 +0200

    Add @Entity Task and Task Crud Repository

--- src/main/java/com/sn/app/controller/MainController.java
+++ src/main/java/com/sn/app/controller/MainController.java
@@ -1,14 +1,24 @@
 package com.sn.app.controller;
 
+import java.util.Date;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.sn.app.domain.Task;
+import com.sn.app.domain.TaskRepository;
 
 @Controller
 public class MainController {
 
+	@Autowired
+	private TaskRepository repo;
+
 	@GetMapping("/")
 	public String home() {
+		Task dd = repo.save(new Task("test", "content test", new Date(), false));
+
 		return "home";
 	}
 }
