da032752823488861b72850c1ee4b5b656a57448
SnYoussef <snyoussef07@gmail.com>
2019-06-15 00:26:58 +0200

    Add feature to add a Task

--- src/main/java/com/sn/app/controller/MainController.java
+++ src/main/java/com/sn/app/controller/MainController.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.sn.app.domain.Task;
 import com.sn.app.domain.TaskRepository;
@@ -18,11 +20,22 @@ public class MainController {
 
 	@GetMapping("/")
 	public String home(Model model) {
-		Task dd = repo.save(new Task("admin", "content test", new Date(), false));
-		Task dd2 = repo.save(new Task("lol", "content test", new Date(), false));
+		/*
+		 * Task dd = repo.save(new Task("admin", "content test", new Date(), false));
+		 * Task dd2 = repo.save(new Task("lol", "content test", new Date(), false));
+		 */
 
 		model.addAttribute("list", repo.findByUser("admin"));
 
 		return "home";
+	}
+
+	@PostMapping("/")
+	public String homeSubmit(@RequestParam String content) {
+		// Task dd = repo.save(new Task("admin", "content test", new Date(), false));
+		// Task dd2 = repo.save(new Task("lol", "content test", new Date(), false));
+		repo.save(new Task("admin", content, new Date(), false));
+
+		return "redirect:/";
 	}
 }
--- src/main/webapp/WEB-INF/views/home.jsp
+++ src/main/webapp/WEB-INF/views/home.jsp
@@ -7,6 +7,14 @@
 <title>Insert title here</title>
 </head>
 <body>
+	<h1>Ajouter une Tache</h1>
+	<form action="/" method="POST">
+		<input name="content" type="text"> <input type="submit"
+			value="Ajouter">
+	</form>
+	<hr>
+
+
 	<h1>Liste des taches a effectuées :</h1>
 	<p>${list }</p>
 </body>
