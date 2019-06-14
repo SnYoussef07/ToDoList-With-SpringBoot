748b53c5798d9beb5c09d3ce1dd5f5b0008b3970
SnYoussef <snyoussef07@gmail.com>
2019-06-14 22:35:34 +0200

    Config for use jsp

--- src/main/java/com/sn/app/controller/MainController.java
+++ src/main/java/com/sn/app/controller/MainController.java
@@ -8,8 +8,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class MainController {
 
 	@GetMapping("/")
-	@ResponseBody
 	public String home() {
-		return "Hello World";
+		return "home";
 	}
 }
